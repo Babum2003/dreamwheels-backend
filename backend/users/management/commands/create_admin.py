f19d4a452f4f7910f91f50e2ea4ff04fe62e5c59
Babum2003 <babum73392@gmail.com>
2026-04-25 14:01:57 +0530

    fix admin with phone number

--- backend/users/management/commands/create_admin.py
+++ backend/users/management/commands/create_admin.py
@@ -5,13 +5,15 @@ import os
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        email = os.getenv('ADMIN_EMAIL', 'admin@sakthibalancars.com')
+        phone_number = os.getenv('ADMIN_PHONE', '9999999999')
         password = os.getenv('ADMIN_PASSWORD', 'Admin@123')
         name = os.getenv('ADMIN_USERNAME', 'admin')
-        if not User.objects.filter(email=email).exists():
-            user = User.objects.create_superuser(email=email, password=password)
-            user.name = name
+        if not User.objects.filter(phone_number=phone_number).exists():
+            user = User(phone_number=phone_number, name=name)
+            user.set_password(password)
             user.is_admin = True
+            user.is_staff = True
+            user.is_superuser = True
             user.save()
             self.stdout.write('Admin created!')
         else:
